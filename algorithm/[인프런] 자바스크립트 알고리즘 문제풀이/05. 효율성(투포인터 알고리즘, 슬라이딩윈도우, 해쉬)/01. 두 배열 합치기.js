238dcf08989548e74b165f49d56cadeee6f75832
Geonsik-Kim <web.gskim@gmail.com>
2022-01-24 21:37:06 +0900

    [2022-01-24] 두 배열 합치기

---
+++ algorithm/[인프런] 자바스크립트 알고리즘 문제풀이/05. 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/01. 두 배열 합치기.js
@@ -0,0 +1,22 @@
+// 길이가 짧은 배열이 끝날 때 까지 서로 비교하여 push를 하고
+// 길이가 짧은 배열이 끝나면 긴 쪽에 원소를 순서대로 push 한다.
+function solution(arr1, arr2) {
+    let answer = [];
+    let n = arr1.length;
+    let m = arr2.length;
+    let p1 = 0;
+    let p2 = 0;
+
+    while (p1 < n && p2 < m) {
+        if (arr1[p1] <= arr2[p2]) answer.push(arr1[p1++]);
+        else answer.push(arr2[p2++]);
+    }
+
+    while (p1 < n) answer.push(arr1[p1++]);
+    while (p2 < n) answer.push(arr1[p2++]);
+    return answer;
+}
+
+let a = [1, 3, 5];
+let b = [2, 3, 6, 7, 9];
+console.log(solution(a, b));
